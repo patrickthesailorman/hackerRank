0d5c7570233b1a2593e1d2606cb6e064e1bc2a14
Patrick McCarty <patrickthesailorman@gmail.com>
2018-03-28 23:16:59 +0000

    Add dataTypes

---
+++ datatypes.js
@@ -0,0 +1,15 @@
+    // Declare second integer, double, and String variables.
+    var integer = 0;
+    var double = 0.0;
+    var string = "";
+    // Read and save an integer, double, and String to your variables.
+    integer = parseInt(readLine());
+    double = parseFloat(readLine());
+    string = readLine();
+    // Print the sum of both integer variables on a new line.
+    console.log(parseInt(integer + i));
+    // Print the sum of the double variables on a new line.
+    console.log(parseFloat(double+d).toFixed(1));
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    console.log(s+string);
