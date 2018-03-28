c3f1faa39c57b6bdcaf852eba8fb7f88b8e55b61
Patrick McCarty <patrickthesailorman@gmail.com>
2018-03-28 23:20:41 +0000

    Add conditional

---
+++ IntroToConditionalStatements.js
@@ -0,0 +1,21 @@
+function main() {
+    var n = parseInt(readLine());
+     (n>=1 && n<=100)
+    {
+        if(n%2!=0){
+            console.log("Weird");
+        }
+        else if 
+            ((n%2==0) && (n < 5) && (n > 2)) {
+            console.log("Not Weird");
+        } 
+        else if 
+            (n%2==0 && n >= 6 && n <= 20) {
+            console.log("Weird");
+        }
+        else 
+            if (n%2==0 && n >20) {
+            console.log("Not Weird");
+        }
+    }    
+}
