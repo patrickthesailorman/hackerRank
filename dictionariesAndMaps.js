342f24aba98dcb9cc6cf211785a893e5b1fdc9f6
Patrick McCarty <patrickthesailorman@gmail.com>
2018-03-29 23:20:52 +0000

    Add dictionaries...

---
+++ dictionariesAndMaps.js
@@ -0,0 +1,11 @@
+function processData(input) {
+    var phoneBook = {
+        name: String,
+        number: Number
+    }
+   input.split(" ");
+    for (var i = 0; i < input.length; i++) {
+        var entries = input[0];
+    }
+    console.log(input);
+} 
