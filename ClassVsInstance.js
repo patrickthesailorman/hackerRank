260dbdb134961a82e0ad48e66b346a5c3a373aeb
Patrick McCarty <patrickthesailorman@gmail.com>
2018-03-29 00:51:47 +0000

    Add Class

---
+++ ClassVsInstance.js
@@ -0,0 +1,35 @@
+function Person(initialAge){
+    // Add some more code to run some checks on initialAge
+    // constructor(initialAge);
+    // let age = initialAge;
+  
+    if (initialAge >= 0) {
+        this.age = initialAge;
+    } else {
+        console.log("Age is not valid, setting age to 0.")
+        this.age = 0;
+    }
+  this.amIOld=function(){
+   // Do some computations in here and print out the correct statement to the console
+    if(this.age < 13) {
+        console.log("You are young.")
+    }
+      
+      else if 
+          (this.age >= 13 && this.age < 18) {
+          console.log("You are a teenager.")
+      }
+      // else if 
+      //     (age >= 19) {
+      //     console.log("You are old.")
+      // }
+      else {
+          console.log( "You are old.")
+      }
+  };
+   this.yearPasses=function(){
+          // Increment the age of the person in here
+       this.age++;
+       
+   };
+}
