c2380835f5fe816204a803e9c1d5df39b499d66c
Patrick McCarty <patrickthesailorman@gmail.com>
2018-03-29 23:22:06 +0000

    Add recursion

---
+++ recursion.js
@@ -0,0 +1,15 @@
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function factorial(n) {
+    // Complete this function
+}
+
+function main() {
+    var n = parseInt(readLine());
+    var result = factorial(n);
+    p
